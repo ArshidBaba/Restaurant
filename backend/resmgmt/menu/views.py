b9f949dd2d4e2ea035de51bc503f802ba2df4d79
Arshid Baba <arshidbaba93@gmail.com>
2022-04-21 18:42:56 +0530

    added api views

--- backend/resmgmt/menu/views.py
+++ backend/resmgmt/menu/views.py
@@ -1,14 +1,8 @@
-from django.http import JsonResponse
-from django.shortcuts import render
-# from .models import Menu
+from rest_framework import generics
 
-# Create your views here.
+from .models import Menu
+from .serializers import MenuSerializer
 
-# def api_home(request, *args, **kwargs):
-#     model_data = Menu.objects.all().order_by("?").first()
-#     data = {}
-#     if model_data:
-#         data['title'] = model_data.title
-#         data['content'] = model_data.content
-#         data['price'] = model_data.price
-#     return JsonResponse(data)
+class MenuDetailAPIView(generics.RetrieveAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
