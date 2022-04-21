1aac09f8e8821b9869cb53ecc19b48ac92503904
Arshid Baba <arshidbaba93@gmail.com>
2022-04-21 16:32:00 +0530

    changed settings

--- backend/resmgmt/api/views.py
+++ backend/resmgmt/api/views.py
@@ -5,15 +5,18 @@ from rest_framework.response import Response
 from menu.models import Menu
 from rest_framework.decorators import api_view
 
+from menu.serializers import MenuSerializer
+
 @api_view(["GET"])
 def api_home(request, *args, **kwargs):
     """
     DRF API View
     """
-    model_data = Menu.objects.all().order_by("?").first()
+    instance = Menu.objects.all().order_by("?").first()
     data = {}
-    if model_data:
-        data = model_to_dict(model_data, fields=['id', 'title', 'price'])
+    if instance:
+        # data = model_to_dict(model_data, fields=['id', 'title', 'price' 'sale_price'])
+        data = MenuSerializer(instance).data
     return Response(data)
     # model_data = Menu.objects.all().order_by("?").first()
     # data = {}
